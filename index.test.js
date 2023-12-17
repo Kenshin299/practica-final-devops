7fe90914711bd5c4171d62fdc68b404d254222c0
Kemyl Fernandez <kemylfernandez299@gmail.com>
2023-12-16 22:54:39 -0400

    updated test

--- index.test.js
+++ index.test.js
@@ -1,19 +1,18 @@
 const fs = require('fs');
 const path = require('path');
-const { JSDOM } = require('jsdom');
-
-const html = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf8');
-
-const dom = new JSDOM(html);
-global.document = dom.window.document;
 
 test('La pagina contiene el titulo y nombre del autor correctos!', () => {
-    const titleElement = document.getElementById('main-title');
-    const authorElement = document.getElementById('author-name');
+    const html = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf8');
 
-    expect(titleElement).not.toBeNull();
-    expect(authorElement).not.toBeNull();
+    // Your test logic here
+    const titleMatch = /<h1[^>]*>([^<]+)<\/h1>/i.exec(html);
+    const authorMatch = /<h2[^>]*>([^<]+)<\/h2>/i.exec(html);
 
-    expect(titleElement.textContent).toBe('Practica Final DevOps');
-    expect(authorElement.textContent).toBe('Kemyl Fernandez');
+    // Check if the elements exist
+    expect(titleMatch).toBeTruthy();
+    expect(authorMatch).toBeTruthy();
+
+    // Check the content of the elements
+    expect(titleMatch[1].trim()).toBe('Practica Final DevOps');
+    expect(authorMatch[1].trim()).toBe('Kemyl Fernandez');
 });
